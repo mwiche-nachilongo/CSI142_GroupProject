97c32aaf8f0e72528c74a1c0db4a23eb5c231afd
mwiche-nachilongo <dinachilongo@gmail.com>
2025-04-17 13:36:09 +0200

    Update Price.java

--- Price.java
+++ Price.java
@@ -1,10 +1,19 @@
+/**Filename: Price
+ * @author: Kyle Makunga 
+ *@version: 1.0
+ * Program to: Display the creation of class for inheritance
+ * To compile: javac Price.java
+ *To execute: java Price
+ */
+
 public class Price {
     private double pricePerKg;
 
     public Price(double pricePerKg) {
-        this.pricePerKg = pricePerKg;
+        this.pricePerKg = pricePerKg; //constructor to inilitailize product attribute (price)
     }
 
+    //getters and setters
     public double getPricePerKg() {
         return pricePerKg;
     }
@@ -12,7 +21,7 @@ public class Price {
     public void setPricePerKg(double pricePerKg) {
         this.pricePerKg = pricePerKg;
     }
-
+//method to calculate price
     public double calculateTotal(double weight) {
         return pricePerKg * weight;
     }
