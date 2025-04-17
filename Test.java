27cafeb1b3a99bc29ea21bc7fd53d77c3a5ce79b
mwiche-nachilongo <dinachilongo@gmail.com>
2025-04-17 12:46:01 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -14,26 +14,26 @@ public class Test {
     Scanner till = new Scanner (System.in);
 
     Fruit f1 = new Fruit("apple", 800.0, false);
-    Meat m1 = new Meat("beef", 400.0, "loin");
+    Meat m1 = new Meat("beef", 400.0, "loin");      //instantiate objects from sub-classes
     Fruit f2= new Fruit("banana", 600.00, false);
     Meat m2 = new Meat("pork", 700.00, "belly");
 
     System.out.println("***Catalogue of items in stock***");
 
     f1.displayInfo();
-    f2.displayInfo();
+    f2.displayInfo();  //Print out objects instantiated
     m1.displayInfo();
     m2.displayInfo();
 
 
-   System.out.println("Select desired weight of fruit or meat in catalogue to validate: ");
+   System.out.println("Select desired weight of fruit or meat in catalogue to validate: "); //Prompt user input
    double weight = till.nextDouble();
    weightValidation(weight);
  
    till.close();
    }
-
-   private static void weightValidation( double weight) throws LimitExceededException {
+//Create method to validate whether the parameter (weight) meets limit criterion
+   private static void weightValidation( double weight) throws LimitExceededException {  
       if (weight < 10)  {} 
       throw new LimitExceededException("Select a weight below 10kg please!");
    }
