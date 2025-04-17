13046227baf0c886a75f6c38c0b9108303b179ac
mwiche-nachilongo <dinachilongo@gmail.com>
2025-04-17 12:52:38 +0200

    Update Meat.java

--- Meat.java
+++ Meat.java
@@ -1,11 +1,18 @@
+/**Filename: Meat
+ * @author: Kyle Makunga 
+ *@version: 1.0
+ * Program to: Display the creation of subclass of Product superclass
+ * To compile: javac Meat.java
+ */
+
 public class Meat extends Product {
     private String cutType;
 
     public Meat(String name, double weight, String cutType) {
-        super(name, weight);
+        super(name, weight);  //Create constructor with subclass attributes
         this.cutType = cutType;
     }
-
+    //getters and setters
     public String getCutType() {
         return cutType;
     }
@@ -13,8 +20,8 @@ public class Meat extends Product {
     public void setCutType(String cutType) {
         this.cutType = cutType;
     }
-
-    @Override
+//Override method in superclass
+    @Override 
     public void displayInfo() {
         System.out.println("Meat: " + getName() + ", Weight: " + getWeight() + "kg, Cut: " + cutType);
     }
