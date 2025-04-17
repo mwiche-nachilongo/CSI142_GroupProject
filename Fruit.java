8362f94787149cb1e3f6144da03f9a2aef50d346
mwiche-nachilongo <dinachilongo@gmail.com>
2025-04-17 12:57:20 +0200

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,19 +1,28 @@
+/**Filename: Fruit
+ * @author: Kyle Makunga 
+ *@version: 1.0
+ * Program to: Display the creation of subclass of superclass (Product)
+ * To compile: javac Fruit.java
+ */
+
 public class Fruit extends Product {
     private boolean isSeasonal;
 
+    //Explicitly delcare product attributes in constructor
     public Fruit(String name, double weight, boolean isSeasonal) {
-        super(name, weight);
+        super(name, weight);  //initialize superclass fields
         this.isSeasonal = isSeasonal;
     }
 
     public boolean isSeasonal() {
         return isSeasonal;
     }
-
+    
+//setter
     public void setSeasonal(boolean seasonal) {
         isSeasonal = seasonal;
     }
-
+//Method from superclass for overridding
     @Override
     public void displayInfo() {
         System.out.println("Fruit: " + getName() + ", Weight: " + getWeight() + "kg, Seasonal: " + isSeasonal);
